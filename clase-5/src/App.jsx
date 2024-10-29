7c446339f199fee4e484e296059375888a5da3ab
Humberto Rivero <humbertorivero9@gmail.com>
2024-10-29 20:19:25 -0300

    Add ejercicio clase 5

--- clase-5/src/App.jsx
+++ clase-5/src/App.jsx
@@ -1,12 +1,22 @@
-import './App.css'
+import "./App.css";
+import {DATA} from "./data";
 
 function App() {
-
-  return (
-    <main>
-      <h1>Conociendo los hooks en React</h1>
-    </main>
-  )
+	return (
+		<main>
+			<h1>Conociendo los hooks en React</h1>
+			<section>
+				{DATA.map((item) => (
+					<div className='card' key={item.id}>
+						<img src={item.image} alt={item.name} />
+						<h2>{item.name}</h2>
+            <p>$ {item.price}</p>
+					</div>
+				))}
+			</section>
+		</main>
+	);
 }
 
-export default App
+export default App;
+
