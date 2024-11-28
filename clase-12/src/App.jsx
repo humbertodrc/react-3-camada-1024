0b97c42e22175bc10c5b65b7c14ae607881c6356
Humberto Rivero <humbertorivero9@gmail.com>
2024-11-28 19:26:13 -0300

    Add ejercicio para los test

--- clase-12/src/App.jsx
+++ clase-12/src/App.jsx
@@ -1,19 +1,13 @@
 import {BrowserRouter, Route, Routes} from "react-router";
-import Home from "./pages/home/Home";
-import Cart from "./pages/cart/Cart";
-import {CartProvider} from "./context/CartContext";
+import Login from "./pages/login/Login";
 
 function App() {
 	return (
-		<CartProvider>
-			<BrowserRouter>
-				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="/cart" element={<Cart />} />
-					<Route path="*" element={<h1>Not Found</h1>} />
-				</Routes>
-			</BrowserRouter>
-		</CartProvider>
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Login />} />
+			</Routes>
+		</BrowserRouter>
 	);
 }
 
